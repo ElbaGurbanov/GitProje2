85c6589d6ffe79d0a0650d1fad6cc76328c96f19
ElbaGurbanov <115432757+ElbaGurbanov@users.noreply.github.com>
2022-10-12 00:25:48 +0200

    ikinci taskimi yapdim

--- src/Gun01/Comment.java
+++ src/Gun01/Comment.java
@@ -1,4 +1,14 @@
 package Gun01;
 
 public class Comment {
+    public static void main(String[] args) {
+        System.out.println("Merhaba 2");
+
+        for (int i=0;i<5;i++);
+        {
+            System.out.println("Merhaba");
+        }
+
+
+    }
 }
