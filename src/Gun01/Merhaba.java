6111c41c1c3f7c1eb5268e7add8b617ce6942b0e
ElbaGurbanov <115432757+ElbaGurbanov@users.noreply.github.com>
2022-10-12 22:04:26 +0200

    ucuncu taskimi yapdim

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -3,6 +3,12 @@ package Gun01;
 public class Merhaba {
     public static void main(String[] args) {
         System.out.println("Merhaba Dünya");
+        int sayac=0;
+        do {
+            System.out.println("yeni merhaba");
+            sayac++;
+
+        } while (sayac<5);
 
 
     }
